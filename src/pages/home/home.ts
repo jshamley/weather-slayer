fb5a0aae84250c367e242bf9d7cc4c2296f0d520
Jeff Shamley <jshamley@comcast.net>
2017-01-05 09:26:22 -0700

    homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public zipcode: number;
 
   constructor(public navCtrl: NavController) {
+    this.getCurrentLocation();
+  }
 
+  searchWeather(e: Event, zipcode: number) {
+    console.log(e, zipcode, this.zipcode);
+  }
+
+  getCurrentLocation() {
+    Geolocation.getCurrentPosition()
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
 }
